4c9e59751943e6395c71980ebdd273158be15435
zurbash <duta1101@bk.ru>
2022-09-27 21:09:49 +0400

    feat: fetching all countries covid stats

--- app/Console/Commands/FetchCountriesStatistic.php
+++ app/Console/Commands/FetchCountriesStatistic.php
@@ -2,41 +2,47 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Stats;
 use Illuminate\Support\Facades\Http;
-
 use Illuminate\Console\Command;
 
 class FetchCountriesStatistic extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'fetch:stats';
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 'fetch:stats';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'fetch covid data of all countries';
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'fetch covid data of all countries';
 
-    /**
-     * Execute the console command.
-     *
-     * @return int
-     */
-    public function handle()
-    {
-        $response = Http::get('https://devtest.ge/countries')->json();
-
-        foreach($response as $country){
-            $stats = Http::post('https://devtest.ge/countries', [
-                'code' => $country
-            ])->json();
-            $data = array_push($data, $stats);
-        }
-        
-    }
+	/**
+	 * Execute the console command.
+	 *
+	 * @return int
+	 */
+	public function handle()
+	{
+		$response = Http::get('https://devtest.ge/countries')->json();
+		$data = [];
+		foreach ($response as $country)
+		{
+			$stats = Http::post('https://devtest.ge/get-country-statistics', [
+				'code' => $country['code'],
+			])->json();
+			Stats::create([
+				'country'   => json_encode($country['name']),
+				'code'      => $stats['code'],
+				'confirmed' => $stats['confirmed'],
+				'recovered' => $stats['recovered'],
+				'death'     => $stats['deaths'],
+			]);
+		}
+	}
 }
--- database/migrations/2022_09_27_110100_create_stats_table.php
+++ database/migrations/2022_09_27_110100_create_stats_table.php
@@ -4,33 +4,32 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('stats', function (Blueprint $table) {
-            $table->id();
-            $table->string('country');
-            $table->string('code');
-            $table->string('confirmed');
-            $table->string('recovered');
-            $table->string('death');
-            $table->timestamps();
-        });
-    }
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('stats', function (Blueprint $table) {
+			$table->id();
+			$table->json('country');
+			$table->string('code');
+			$table->integer('confirmed');
+			$table->integer('recovered');
+			$table->integer('death');
+			$table->timestamps();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('stats');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('stats');
+	}
 };
