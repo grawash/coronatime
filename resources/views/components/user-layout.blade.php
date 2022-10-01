edbf70c6f7c4d864f0d7f3d1e0c5aedb4b69678a
zurbash <duta1101@bk.ru>
2022-10-01 10:36:58 +0400

    feat: responsive login page

--- resources/views/components/user-layout.blade.php
+++ resources/views/components/user-layout.blade.php
@@ -3,16 +3,18 @@
         <div class="grow-[3] basis-0">
             <!-- use sm,md,lg... media selectors for larger devices, tailwind targets mobile devices on default!! example below -->
             <div class="flex">
-                <div class="lg:mt-14 ml-32 mt-4 w-fit">
-                    <img src="./images/Group 1.svg" alt="">
+                <div class="lg:mt-14 ml-4 mr-4 lg:mr-0 md:ml-32 mt-4 w-fit">
+                    <div class="flex">
+                        <img src="./images/Group 1.svg" alt="">
+                        <x-select-language class="ml-auto mr-7"/>
+                    </div>
                     {{ $slot }}
                 </div>
-                <x-select-language class="ml-auto lg:mt-14 mt-4 lg:mr-14"/>
             </div>
 
 
         </div>
-        <div class="bg-vials bg-no-repeat bg-cover grow-[2] basis-0">
+        <div class="hidden md:block bg-vials bg-no-repeat bg-cover grow-[2] basis-0">
         </div>
     </div>
 </x-layout>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,13 +2,13 @@
     <form method="POST" action="{{ route('login') }}" class="mt-16">
         @csrf
         <h1 class="text-2xl font-[1000]">{{__('login.welcome') }}</h1>
-        <p class="text-neutral-600 text-xl mt-4">{{__('login.welcome_text') }}</p>
+        <p class="text-neutral-600 text-base md:text-xl mt-4">{{__('login.welcome_text') }}</p>
         <x-form.input name="username" type="text" placeholder="{{__('login.username_placeholder') }}" required />
         <x-form.input name="password" type="password" placeholder="{{__('login.password_placeholder') }}" required />
         <div class="flex mt-6 items-center">
             <input type="checkbox" id='remember'>
-            <label for="remember" class="pl-2 font-semibold text-sm">{{__('login.remember') }}</label>
-            <a href="{{ route('password.request') }}" class="ml-auto text-blue-700 font-semibold text-sm">{{__('login.forgot_password') }}</a>
+            <label for="remember" class="pl-2 font-semibold text-xs md:text-sm">{{__('login.remember') }}</label>
+            <a href="{{ route('password.request') }}" class="ml-auto text-blue-700 font-semibold text-xs md:text-sm">{{__('login.forgot_password') }}</a>
         </div>
         <button type="submit" class="bg-green-500 hover:bg-green-600 mt-6 w-full h-14 text-white text-base font-[900] rounded-md">{{__('login.login') }}</button>
         <div class="flex mt-6 items-center justify-center">
